55bc9a3a87ef3871e1f0abfffcc7fb7370826218
Christopher-Polloni <christopher.polloni16@stjohns.edu>
2019-02-22 09:22:40 -0500

    first commit

---
+++ playground/models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Course(db.Model):
+    __tablename__="courses"
+    id=db.Column(db.Integer,primary_key=True)
+    course_number=db.Column(db.String, nullable=False)
+    course_title=db.Column(db.String, nullable =False)
+    students = db.relationship("RegisteredStudent", backref="courses", lazy=True)
+
+    def add_student(self,name,grade):
+        newStudent = RegisteredStudent(name=name, grade=grade, course_id=self.id )
+        db.session.add(newStudent)
+        db.session.commit()
+
+class RegisteredStudent(db.Model):
+    __tablename__="students"
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String, nullable=False)
+    grade=db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
